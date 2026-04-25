eb9e418556f81c65ae63d6719fe73346a2b186dd
Clwadsworth <clwadsworth93@byui.edu>
2026-04-25 00:12:07 -0600

    Prep 1 all program finished

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
@@ -10,5 +11,43 @@ class Program
         string last = Console.ReadLine();
 
         Console.WriteLine($"Your name is {last}, {first} {last}.");
+
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is {letter}.");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed the class!!!!"):
+        }
+        else
+        {
+            Console.WriteLine("Try again next time.");
+        }
     }
 }
