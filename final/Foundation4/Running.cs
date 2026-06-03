f9418ae3d56f0ae9b8208f60a54a0d7940019fb0
Clwadsworth <clwadsworth93@byui.edu>
2026-06-03 17:08:51 -0600

    Running Class built

--- final/Foundation4/Running.cs
+++ final/Foundation4/Running.cs
@@ -1,6 +1,30 @@
 public class Running : Activity
 {
     private double _distanceRan;
+    // main function
+    public Running(string date, int minutes, double distance) : base(date, minutes)
+    {
+        _distanceRan = distance;
+    }
+    // all of our override statements needed for the activity class
+    public override double GetDistance()
+    {
+        return _distanceRan;
+    }
 
+    public override double GetSpeed()
+    {
+        return (_distanceRan / GetMinutes()) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return GetMinutes() / _distanceRan;
+    }
+    // complete summary for running
+    public override string GetSummary()
+    {
+        return $"{GetDate()} \nRan for: {GetMinutes()} \nFor a distance of: {GetDistance():F1} miles \nRunning at: {GetSpeed():F1} mph \nAt a pacing of: {GetPace():F1} minutes per mile";
+    }
     
 }
